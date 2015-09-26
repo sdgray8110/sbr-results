e7f01b1a9cb11dc6bd09d07b641034e256f3d1f2
Spencer Gray <spencer@mapletonhillmedia.com>
2015-09-25 20:19:10 -0600

    Adding favicon

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -9,6 +10,7 @@ var routes = require('./routes/index');
 var app = express();
 
 // view engine setup
+app.use(favicon(__dirname + '/public/images/favicon.png'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
